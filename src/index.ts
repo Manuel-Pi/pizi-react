7711da5d1c98c0da124331d8bb8f4200f9333177
Manuel-Pi <psykofou@hotmail.com>
2021-04-23 00:50:59 -0400

    update

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./main.less"
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import { App } from "./App"
+import { Table } from "./components/Table/Table";
+
+
+//ReactDOM.render(<App />, document.getElementsByTagName("app")[0]);
+
+export {Table};
